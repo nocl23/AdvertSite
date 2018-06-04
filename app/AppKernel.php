8391e5a080366a30e0feb74996b3aeb0e85c30bc
Noémie CLAY <noemie.clay@estiam.com>
2018-06-05 00:27:21 +0200

    Add boostrap module

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,8 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
             new AdvertSiteBundle\AdvertSiteBundle(),
+            new Symfony\Bundle\AsseticBundle\AsseticBundle(),
+
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
